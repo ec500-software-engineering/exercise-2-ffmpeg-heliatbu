50cb67db6bfb62e758d18d427c618d47e9b1ed3d
heliatbu <47123410+heliatbu@users.noreply.github.com>
2019-03-03 14:21:24 -0500

    Add files via upload

---
+++ test_duration.py
@@ -0,0 +1,26 @@
+import subprocess
+import json
+from pathlib import Path
+from pytest import approx
+
+def ffprobe(filein: Path) -> dict:
+    """ get media metadata """
+    meta_json = subprocess.check_output([
+		'ffprobe', '-v', 'warning', '-print_format',
+		'json', '-show_streams', '-show_format', filein],
+    universal_newlines = True
+	)
+    return json.loads(meta_json)
+
+def test_duration():
+    fnin = 'video.mp4'
+    fnout = 'video_480p.mp4'
+    fnout2 = 'video_720p.mp4'
+    orig_meta = ffprobe(fnin)
+    orig_duration = float(orig_meta['streams'][0]['duration'])
+    meta_480 = ffprobe(fnout)
+    duration_480 = float(meta_480['streams'][0]['duration'])
+    meta_720 = ffprobe(fnout2)
+    duration_720 = float(meta_720['streams'][0]['duration'])
+    assert orig_duration == approx(duration_480) == approx(duration_720)
+
